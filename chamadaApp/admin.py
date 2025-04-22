a4e875684085861e2909d93d6fb87fcb35f6d2e9
aluno <amandasilvademoraes30@gmail.com>
2025-04-22 20:54:04 -0300

    versao 1.0

--- chamadaApp/admin.py
+++ chamadaApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from chamadaApp.models import Cliente,Plano,Telefone,Categoria
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Plano)
+admin.site.register(Telefone)
+admin.site.register(Categoria)
